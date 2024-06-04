c2e60be59dada54f2ecc4abe7b14c2417574ca6d
FredoKenricWijaya <fredowijaya36@gmail.com>
2024-06-04 14:04:51 +0700

    RailwayTestings

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
