3e52c742bb0bf4f52654a35c8e9cd5d894abdc36
FredoKenricWijaya <fredowijaya36@gmail.com>
2024-06-12 10:21:33 +0700

    UserUpdate

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers;
 
     /**
- * @OA\Info(title="PHP Admin Dashboard", version="1.0")
+ * @OA\Info(
+ * title="PHP Admin Dashboard",
+ * version="1.0",
+ *     *      @OA\Contact(
+    *          email="Fredowijaya36@gmail.com"
+    *      ),
+ * )
   * @OA\Server(
  *     url="http://groupproject-production.up.railway.app/api",
  *     description="API Server"
